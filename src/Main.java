1f382d7172df8d8991d9fcd5a660c7df2088588a
Elmira <elmirasco@gmail.com>
2020-01-22 11:42:22 +0200

    Изменила комментарии в классе Main и ввела два поля: countTree и countNode

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 /*
-Двоичное дерево поиска. (Исправила ошибку)
-Изменила метод isBalance, теперь несбалансированных деревьев в среднем получается 55 % - 80 %
+Двоичное дерево поиска.
+Метод isBalance: несбалансированных деревьев получается 95 - 100 проц.,
+если оценивать строго, balance(node) < 2.
 */
 public class Main {
     public static void main(String[] args) {
 
         Random random = new Random();
-        double countFalse = 0; // для подсчета
+        int countTree = 20; // кол-во деревьев
+        int countNode = 64; // кол-во добавляемых узлов в каждое дерево
+        double countFalse = 0; // для подсчета несбалансированных деревьев
 
 
         List<MyTree> listTree = new ArrayList<>();
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < countTree; i++) {
             MyTree tree = new MyTree();
-            for (int j = 0; j < 64 ; j++) {
+            for (int j = 0; j < countNode ; j++) {
                 tree.insert(random.nextInt(201) - 100);
             }
             listTree.add(tree);
@@ -29,7 +32,7 @@ public class Main {
             System.out.println(b);
         }
 
-        System.out.println(countFalse/20 + " - доля несбалансированных деревьев");
+        System.out.println(countFalse/countTree + " - доля несбалансированных деревьев");
 
 // Обход деревьев
 //        for (MyTree tree: listTree) {
